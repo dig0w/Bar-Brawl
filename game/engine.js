e0ba4a53e747fa1449660b6d345ae1bf470256e3
dig0w <dig0w.yt@gmail.com>
2026-06-18 15:58:07 +0100

    Intro sounds

--- game/engine.js
+++ game/engine.js
@@ -39,7 +39,8 @@ export class FighterEngine {
     #scoreF1 = 0;
 
     static IntroSheet = Object.assign(new Image(), { src: "assets/intro.webp" });
-    static frameStamp = [ 1, 1.15, 1.3, 1.45, 1.6, 3, 3.05, 3.1, 3.15, 3.2, 3.25, 4.75, 6, 6.15, 6.3, 6.45, 6.6, 7.1 ];
+    static frameStamp = [  1,  1.15, 1.3, 1.45, 1.6, 3,  3.05, 3.1, 3.15, 3.2, 3.25, 4.75, 6.15,  6.3, 6.45, 6.6, 6.75, 7.25 ];
+    static frameSfx =   [ -1, -1,   -1,  -1,   -1,   7, -1,   -1,  -1,   -1,  -1,    8,    9,    -1,  -1,   -1,  10,   -1    ];
     #introTimer = 0;
     static maxIntroFramesLine = 6;
     static maxIntroState = 18;
@@ -116,6 +117,10 @@ export class FighterEngine {
         "assets/hit_2.ogg",
         "assets/groan_1.ogg",
         "assets/groan_2.ogg",
+        "assets/intro_line1.ogg",
+        "assets/intro_line2.ogg",
+        "assets/intro_line3.ogg",
+        "assets/bottle_breaking.ogg",
     ]
     #soundBuffers = [];
     #volume = .5;
@@ -286,6 +291,8 @@ export class FighterEngine {
                 } 
                 else if (this.#introState < FighterEngine.maxIntroState - 1) {
                     this.#introState++;
+
+                    this.PlaySound(FighterEngine.frameSfx[this.#introState]);
                 }
             }
 
